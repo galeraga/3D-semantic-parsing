920524df80eaeddc311f128c780bd663aa16e096
Javier Galera <javier.galera@gmail.com>
2022-04-23 16:39:14 +0200

    Added comment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # pip install open3d
 import open3d as o3d
 
+# To quickly test o3d
 if __name__ == "__main__":
     dataset = o3d.data.EaglePointCloud()
     pcd = o3d.io.read_point_cloud(dataset.path)
